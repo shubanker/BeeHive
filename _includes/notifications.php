37454be50e1d4fb903967d2b48f2b3de5ea8e50f
Shubanker Chourasia <subhankerchourasia@gmail.com>
2015-12-29 00:37:37 +0530

    Fixed notofocation issues(thanks to SO)

--- _includes/notifications.php
+++ _includes/notifications.php
@@ -53,7 +53,7 @@ class Notifications{
 	 */
 	static function get_notifications($user_id,$status=NULL,$db=NULL){
 		$status_condition = $status===0||$status===1?"AND `status`='$status'":"";
-		$sql="SELECT
+		/*$sql="SELECT
 		`notification_id`,
 				CASE 
 				WHEN `msg` IS NOT NULL THEN msg
@@ -61,7 +61,17 @@ class Notifications{
 				WHEN `type`=2 THEN concat((SELECT count(*) from (SELECT DISTINCT `user_id` FROM `comments` WHERE `post_id`=post_id AND `user_id`!='$user_id') AS commenters),' peoples Commented on your post')
 				END AS message
 				FROM
-				`notifications` WHERE user_id='$user_id' $status_condition";
+				`notifications` WHERE user_id='$user_id' $status_condition";*/
+		$sql="SELECT n.notification_id,
+    CASE 
+    WHEN n.msg IS NOT NULL THEN n.msg
+    WHEN type=1 THEN concat((SELECT count(user_id) from likes AS l WHERE l.post_id=n.post_id AND l.type=1 AND l.user_id != n.user_id),' peoples Liked your post')
+    WHEN type=2 THEN concat((SELECT count(DISTINCT c.user_id) FROM comments AS c WHERE c.post_id=n.post_id AND c.USER_ID != n.user_id),' peoples Commented on your post')
+    END AS message
+FROM
+    notifications AS n 
+WHERE 
+    n.user_id='$user_id' $status_condition";
 		return empty($db)?$sql:Db::fetch_array($db, $sql);
 	}
 }
