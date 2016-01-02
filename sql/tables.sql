345746a5c7bdbd03f30f21dbc2a88a52494fc829
Shubanker Chourasia <subhankerchourasia@gmail.com>
2016-01-02 16:24:11 +0530

    Added tables for images,albums

--- sql/tables.sql
+++ sql/tables.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 25, 2015 at 05:28 PM
+-- Generation Time: Jan 02, 2016 at 11:49 AM
 -- Server version: 5.6.20
 -- PHP Version: 5.5.15
 
@@ -19,6 +19,22 @@ SET time_zone = "+00:00";
 --
 -- Database: `social`
 --
+CREATE DATABASE IF NOT EXISTS `social` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `social`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `albums`
+--
+
+CREATE TABLE IF NOT EXISTS `albums` (
+`album_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `access` tinyint(4) NOT NULL DEFAULT '1',
+  `status` tinyint(4) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -33,7 +49,47 @@ CREATE TABLE IF NOT EXISTS `comments` (
   `comment` text NOT NULL,
   `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `status` int(11) NOT NULL DEFAULT '1'
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1  ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
+
+--
+-- Triggers `comments`
+--
+DELIMITER //
+CREATE TRIGGER `add_notification_comment` AFTER INSERT ON `comments`
+ FOR EACH ROW BEGIN
+		DECLARE ui INT DEFAULT 0;
+		SET ui = (SELECT `user_id` from post WHERE post.`post_id`=NEW.`post_id` LIMIT 1);
+		IF(ui != NEW.`user_id`) THEN
+		INSERT INTO `notifications` (`user_id`,`post_id`,`type`,`status`) VALUES(
+			ui,
+			NEW.`post_id`,
+			2,
+			1)  ON DUPLICATE KEY UPDATE status=VALUES(status),time=VALUES(time);
+		END IF;
+	END
+//
+DELIMITER ;
+DELIMITER //
+CREATE TRIGGER `remove_notification_comment` AFTER DELETE ON `comments`
+ FOR EACH ROW BEGIN
+		DECLARE commenters_count INT DEFAULT 0;
+		DECLARE ui INT DEFAULT 0;
+		DELETE FROM `likes` WHERE post_id=OLD.`post_id` AND type=2; -- Removing likes which was on comments ..
+		SET ui=(SELECT `user_id` from post WHERE post.`post_id`=OLD.`post_id` LIMIT 1);
+
+		SET commenters_count = (SELECT count(`comments`.`user_id`) from `comments` WHERE 
+			`comments`.`post_id` = OLD.`post_id` AND 
+			`comments`.`status`=1 AND 
+			`comments`.`user_id`!=ui
+			);
+		IF(commenters_count = 0) THEN
+		DELETE FROM `notifications` WHERE post_id=OLD.`post_id` AND
+		user_id = ui AND 
+		type=2;
+		END IF;
+	END
+//
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -46,7 +102,20 @@ CREATE TABLE IF NOT EXISTS `friends` (
   `user_one` int(11) NOT NULL,
   `user_two` int(11) NOT NULL,
   `status` int(11) NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1  ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `images`
+--
+
+CREATE TABLE IF NOT EXISTS `images` (
+`image_id` int(11) NOT NULL,
+  `album_id` int(11) NOT NULL,
+  `loc` varchar(100) NOT NULL,
+  `status` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -59,6 +128,47 @@ CREATE TABLE IF NOT EXISTS `likes` (
   `post_id` int(11) NOT NULL,
   `type` tinyint(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Triggers `likes`
+--
+DELIMITER //
+CREATE TRIGGER `add_notification_like` AFTER INSERT ON `likes`
+ FOR EACH ROW BEGIN
+		DECLARE ui INT DEFAULT 0;
+		IF(NEW.`type` = 1) THEN
+			SET ui = (SELECT `user_id` from post WHERE post.`post_id`=NEW.`post_id` LIMIT 1);
+			IF(ui != NEW.`user_id`) THEN
+			INSERT INTO `notifications` (`user_id`,`post_id`,`type`,`status`) VALUES(
+				ui,
+				NEW.`post_id`,
+				1,
+				1)  ON DUPLICATE KEY UPDATE status=VALUES(status),time=VALUES(time);
+			END IF;
+		END IF;
+	END
+//
+DELIMITER ;
+DELIMITER //
+CREATE TRIGGER `remove_notification_likes` AFTER DELETE ON `likes`
+ FOR EACH ROW BEGIN
+		DECLARE likes_count INT DEFAULT 0;
+		DECLARE ui INT DEFAULT 0;
+		SET ui=(SELECT `user_id` from post WHERE post.`post_id`=OLD.`post_id` LIMIT 1);
+
+		SET likes_count = (SELECT count(`likes`.`user_id`) from `likes` WHERE 
+			`likes`.`post_id` = OLD.`post_id` AND 
+			`likes`.`type`=1 AND 
+			`likes`.`user_id`!=ui
+			);
+		IF(likes_count = 0) THEN
+		DELETE FROM `notifications` WHERE post_id=OLD.`post_id` AND
+		user_id = ui AND 
+		type=1;
+		END IF;
+	END
+//
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -73,7 +183,23 @@ CREATE TABLE IF NOT EXISTS `messages` (
   `message` text NOT NULL,
   `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `status` tinyint(4) NOT NULL DEFAULT '1'
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1  ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notifications`
+--
+
+CREATE TABLE IF NOT EXISTS `notifications` (
+`notification_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `post_id` int(11) DEFAULT NULL,
+  `msg` varchar(50) DEFAULT NULL,
+  `type` tinyint(4) NOT NULL,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `status` tinyint(4) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -90,7 +216,20 @@ CREATE TABLE IF NOT EXISTS `post` (
   `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `status` int(11) DEFAULT '1',
   `access` int(11) DEFAULT '1'
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1  ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
+
+--
+-- Triggers `post`
+--
+DELIMITER //
+CREATE TRIGGER `remove_notification_post` AFTER UPDATE ON `post`
+ FOR EACH ROW BEGIN
+	IF(New.`status`!=1) THEN
+	DELETE FROM `notifications` WHERE `notifications`.`post_id`=OLD.`post_id`;
+	END IF;
+	END
+//
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -103,8 +242,8 @@ CREATE TABLE IF NOT EXISTS `userdata` (
   `user_id` int(11) NOT NULL,
   `type` varchar(25) NOT NULL,
   `data` text NOT NULL,
-  `status` tinyint(4) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1  ;
+  `status` tinyint(4) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -120,12 +259,18 @@ CREATE TABLE IF NOT EXISTS `users` (
   `password` varchar(100) NOT NULL,
   `gender` char(1) NOT NULL,
   `dob` date NOT NULL,
-  `status` tinyint(4) NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1  ;
+  `status` tinyint(4) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `albums`
+--
+ALTER TABLE `albums`
+ ADD PRIMARY KEY (`album_id`), ADD KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `comments`
@@ -140,6 +285,12 @@ ALTER TABLE `friends`
  ADD PRIMARY KEY (`friend_id`), ADD KEY `user_one` (`user_one`), ADD KEY `user_two` (`user_two`);
 
 --
+-- Indexes for table `images`
+--
+ALTER TABLE `images`
+ ADD PRIMARY KEY (`image_id`), ADD KEY `album_id` (`album_id`);
+
+--
 -- Indexes for table `likes`
 --
 ALTER TABLE `likes`
@@ -150,6 +301,12 @@ ALTER TABLE `likes`
 --
 ALTER TABLE `messages`
  ADD PRIMARY KEY (`message_id`), ADD KEY `user_two` (`user_two`), ADD KEY `user_one` (`user_one`);
+
+--
+-- Indexes for table `notifications`
+--
+ALTER TABLE `notifications`
+ ADD PRIMARY KEY (`notification_id`), ADD UNIQUE KEY `user_id` (`user_id`,`post_id`,`type`), ADD KEY `post_id` (`post_id`), ADD KEY `user_id_2` (`user_id`);
 
 --
 -- Indexes for table `post`
@@ -174,6 +331,11 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `albums`
+--
+ALTER TABLE `albums`
+MODIFY `album_id` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
@@ -184,10 +346,20 @@ MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT;
 ALTER TABLE `friends`
 MODIFY `friend_id` int(11) NOT NULL AUTO_INCREMENT;
 --
+-- AUTO_INCREMENT for table `images`
+--
+ALTER TABLE `images`
+MODIFY `image_id` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `messages`
 --
 ALTER TABLE `messages`
 MODIFY `message_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `notifications`
+--
+ALTER TABLE `notifications`
+MODIFY `notification_id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `post`
 --
@@ -208,6 +380,12 @@ MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
 --
 
 --
+-- Constraints for table `albums`
+--
+ALTER TABLE `albums`
+ADD CONSTRAINT `albums_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+--
 -- Constraints for table `comments`
 --
 ALTER TABLE `comments`
@@ -220,6 +398,13 @@ ADD CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`post_id`) REFERENCES `post` (`pos
 ALTER TABLE `friends`
 ADD CONSTRAINT `friends_ibfk_1` FOREIGN KEY (`user_one`) REFERENCES `users` (`user_id`),
 ADD CONSTRAINT `friends_ibfk_2` FOREIGN KEY (`user_two`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `images`
+--
+ALTER TABLE `images`
+ADD CONSTRAINT `images_ibfk_1` FOREIGN KEY (`album_id`) REFERENCES `albums` (`album_id`),
+ADD CONSTRAINT `images_ibfk_2` FOREIGN KEY (`album_id`) REFERENCES `albums` (`album_id`);
 
 --
 -- Constraints for table `likes`
@@ -235,6 +420,13 @@ ADD CONSTRAINT `messages_ibfk_1` FOREIGN KEY (`user_two`) REFERENCES `users` (`u
 ADD CONSTRAINT `messages_ibfk_2` FOREIGN KEY (`user_one`) REFERENCES `users` (`user_id`);
 
 --
+-- Constraints for table `notifications`
+--
+ALTER TABLE `notifications`
+ADD CONSTRAINT `notifications_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
+ADD CONSTRAINT `notifications_ibfk_2` FOREIGN KEY (`post_id`) REFERENCES `post` (`post_id`);
+
+--
 -- Constraints for table `post`
 --
 ALTER TABLE `post`
@@ -245,145 +437,6 @@ ADD CONSTRAINT `post_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_i
 --
 ALTER TABLE `userdata`
 ADD CONSTRAINT `userdata_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
-
---
--- Table structure for table `notifications`
---
-
-CREATE TABLE IF NOT EXISTS `notifications` (
-`notification_id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) NOT NULL,
-  `post_id` int(11) DEFAULT NULL,
-  `msg` varchar(50) DEFAULT NULL,
-  `type` tinyint(4) NOT NULL,
-  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `status` tinyint(4) NOT NULL DEFAULT '1'
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 ;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `notifications`
---
-ALTER TABLE `notifications`
- ADD PRIMARY KEY (`notification_id`), ADD UNIQUE KEY `user_id` (`user_id`,`post_id`,`type`), ADD KEY `post_id` (`post_id`), ADD KEY `user_id_2` (`user_id`);
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `notifications`
---
-ALTER TABLE `notifications`
-ADD CONSTRAINT `notifications_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
-ADD CONSTRAINT `notifications_ibfk_2` FOREIGN KEY (`post_id`) REFERENCES `post` (`post_id`);
-
-
-/*
-CREATE TRIGGER  add_notification_comment AFTER INSERT ON `comments`
-	FOR EACH ROW
-	INSERT INTO `notifications` (`user_id`,`post_id`,`type`,`status`) VALUES(
-		(SELECT `user_id` from post WHERE post.`post_id`=NEW.`post_id` LIMIT 1),
-		NEW.`post_id`,
-		2,
-		1)  ON DUPLICATE KEY UPDATE status=VALUES(status),time=VALUES(time);
-
-CREATE TRIGGER  add_notification_like AFTER INSERT ON `likes`
-	FOR EACH ROW
-	INSERT INTO `notifications` (`user_id`,`post_id`,`type`,`status`) VALUES(
-		(SELECT `user_id` from post WHERE post.`post_id`=NEW.`post_id` LIMIT 1),
-		NEW.`post_id`,
-		1,
-		1)  ON DUPLICATE KEY UPDATE status=VALUES(status),time=VALUES(time);
-*/
-DELIMITER //
-CREATE TRIGGER  add_notification_comment AFTER INSERT ON `comments`
-	FOR EACH ROW
-	BEGIN
-		DECLARE ui INT DEFAULT 0;
-		SET ui = (SELECT `user_id` from post WHERE post.`post_id`=NEW.`post_id` LIMIT 1);
-		IF(ui != NEW.`user_id`) THEN
-		INSERT INTO `notifications` (`user_id`,`post_id`,`type`,`status`) VALUES(
-			ui,
-			NEW.`post_id`,
-			2,
-			1)  ON DUPLICATE KEY UPDATE status=VALUES(status),time=VALUES(time);
-		END IF;
-	END
-
-
-DELIMITER //
-CREATE TRIGGER  add_notification_like AFTER INSERT ON `likes`
-	FOR EACH ROW
-	BEGIN
-		DECLARE ui INT DEFAULT 0;
-		IF(NEW.`type` = 1) THEN
-			SET ui = (SELECT `user_id` from post WHERE post.`post_id`=NEW.`post_id` LIMIT 1);
-			IF(ui != NEW.`user_id`) THEN
-			INSERT INTO `notifications` (`user_id`,`post_id`,`type`,`status`) VALUES(
-				ui,
-				NEW.`post_id`,
-				1,
-				1)  ON DUPLICATE KEY UPDATE status=VALUES(status),time=VALUES(time);
-			END IF;
-		END IF;
-	END
-DROP TRIGGER IF EXISTS remove_notification_comment; 
-DELIMITER //
-CREATE TRIGGER  remove_notification_comment AFTER DELETE ON `comments`
-	FOR EACH ROW
-	BEGIN
-		DECLARE commenters_count INT DEFAULT 0;
-		DECLARE ui INT DEFAULT 0;
-		DELETE FROM `likes` WHERE post_id=OLD.`post_id` AND type=2; -- Removing likes which was on deleted comment ..
-
-		SET ui=(SELECT `user_id` from post WHERE post.`post_id`=OLD.`post_id` LIMIT 1);
-
-		SET commenters_count = (SELECT count(`comments`.`user_id`) from `comments` WHERE 
-			`comments`.`post_id` = OLD.`post_id` AND 
-			`comments`.`status`=1 AND 
-			`comments`.`user_id`!=ui
-			);
-		IF(commenters_count = 0) THEN
-		DELETE FROM `notifications` WHERE post_id=OLD.`post_id` AND
-		user_id = ui AND 
-		type=2;
-		END IF;
-	END
-
-DROP TRIGGER IF EXISTS remove_notification_likes; 
-DELIMITER //
-CREATE TRIGGER  remove_notification_likes AFTER DELETE ON `likes`
-	FOR EACH ROW
-	BEGIN
-		DECLARE likes_count INT DEFAULT 0;
-		DECLARE ui INT DEFAULT 0;
-		SET ui=(SELECT `user_id` from post WHERE post.`post_id`=OLD.`post_id` LIMIT 1);
-
-		SET likes_count = (SELECT count(`likes`.`user_id`) from `likes` WHERE 
-			`likes`.`post_id` = OLD.`post_id` AND 
-			`likes`.`type`=1 AND 
-			`likes`.`user_id`!=ui
-			);
-		IF(likes_count = 0) THEN
-		DELETE FROM `notifications` WHERE post_id=OLD.`post_id` AND
-		user_id = ui AND 
-		type=1;
-		END IF;
-	END
-
-DROP TRIGGER IF EXISTS remove_notification_post; 
-DELIMITER //
-CREATE TRIGGER  remove_notification_post AFTER UPDATE ON `post`
-	FOR EACH ROW
-	BEGIN
-	IF(New.`status`!=1) THEN
-	DELETE FROM `notifications` WHERE `notifications`.`post_id`=OLD.`post_id`;
-	END IF;
-	END
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
