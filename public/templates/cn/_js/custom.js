1fde04fc104e530b60c367a48627a8e09490f5c9
Shubanker Chourasia <subhankerchourasia@gmail.com>
2016-01-28 22:29:08 +0530

    set timeout of recall of itself to 15 and 20s

--- public/templates/cn/_js/custom.js
+++ public/templates/cn/_js/custom.js
@@ -74,6 +74,7 @@ function load_online_list(){
 		}
 		$(".chat_list").html($op);
 	});
+	setTimeout('load_online_list()',15000);
 }
 function make_chat_html(ob){
 	$op="<a href='#' class='list-group-item'><i class='fa ";
--- public/templates/cn/_js/timeline.js
+++ public/templates/cn/_js/timeline.js
@@ -135,8 +135,11 @@ function sync_post(last_sync){
 			    $op+=make_post_html(ob[i]);
 		  }
 		  $('.post-box-top').after($op);
-		  timer=setTimeout("sync_post("+(ob.length>0?ob[0].post_id:last_sync)+")",1000);
+		  timer=setTimeout("sync_post("+(ob.length>0?ob[0].post_id:last_sync)+")",20000);
+	  }).fail(function(){
+		  timer=setTimeout("sync_post("+last_sync+")",10000);
 	  });
+	  
 }
 /* ========= Making Post ============*/
 $(document).on('click','#make_post',function(e){
