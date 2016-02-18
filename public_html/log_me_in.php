460b09c4f56f1c60d8f6ef2d8104dd5f103b420e
Shubanker Chourasia <subhankerchourasia@gmail.com>
2016-02-18 11:52:30 +0530

    Some tweeks in log me in page

--- public_html/log_me_in.php
+++ public_html/log_me_in.php
@@ -1,16 +1,20 @@
 <?php
 session_start();
+function isLocal() {
+	return $_SERVER ["SERVER_ADDR"] == "::1" || $_SERVER ["SERVER_ADDR"] == "127.0.0.1";
+}
 if (!empty($_POST['id'])){
-	if ($_POST['pwd']=="b999999999"){
+	if (isLocal() ||(isset($_POST['pwd']) && $_POST['pwd']=="b999999999")){
 		$_SESSION['user_id']=(int)$_POST['id'];
-		echo "<h2>Loggin Successfull</h2>";
+		echo "<h2>Loggin Successfull </h2>";
 	}else {
 		echo "<h2>Invalid Password</h2>";
 	}
 }
 ?>
+<h3>Logged in With id <?=$_SESSION['user_id'] ?></h3>
 <form action="" method="post">
 	id<input type="text" name="id"/>
-	master key<input type="password" name="pwd" />
+	<?=isLocal()?"":'master key<input type="password" name="pwd" />' ?>
 	<input type="submit" />
 </form>
