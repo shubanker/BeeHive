15c23b5965e50fa66d3eae5cbc8e67bec39f20f9
Shubanker Chourasia <subhankerchourasia@gmail.com>
2016-04-12 02:38:22 +0530

    removed link redirects

--- public_html/mail_inbox.php
+++ public_html/mail_inbox.php
@@ -77,10 +77,10 @@ if ($time=="Not available"){
             <ul class="nav nav-pills nav-stacked">
                 <li class="active"><a href="#"><span class="badge pull-right">42</span> Inbox </a>
                 </li>
-                <li><a href="http://www.jquery2dotnet.com">Starred</a></li>
-                <li><a href="http://www.jquery2dotnet.com">Important</a></li>
-                <li><a href="http://www.jquery2dotnet.com">Sent Mail</a></li>
-                <li><a href="http://www.jquery2dotnet.com"><span class="badge pull-right">3</span>Drafts</a></li>
+                <li><a href="#">Starred</a></li>
+                <li><a href="#">Important</a></li>
+                <li><a href="#">Sent Mail</a></li>
+                <li><a href="#"><span class="badge pull-right">3</span>Drafts</a></li>
             </ul>
         </div>
         <div class="col-sm-9 col-md-10">
